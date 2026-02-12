af6242a3b2d96cdf784bd6cd28de1cf3062283f2
Evan Gedrich <116127782+evangedrich@users.noreply.github.com>
2026-02-11 19:55:09 -0500

    Study page improvements
    Added functionality for range of sections to be included in the deck of flash cards, a button to return to the previously seen card, automatically moving on to the next card when guessed correctly, and custom SVG symbols for the buttons.

--- src/app/(main)/study/page.tsx
+++ src/app/(main)/study/page.tsx
@@ -1,10 +1,26 @@
 'use client';
 
-import { useState, ChangeEvent, FormEvent } from 'react';
+import React, { useState, useEffect, useRef, ChangeEvent, FormEvent } from 'react';
 import { lessons, phaseType, trilingualType } from '@/app/lib/lessons';
+import { montserrat } from '@/app/ui/fonts';
 import styles from '@/app/ui/study.module.css';
 import Text from '@/app/components/text-prep';
 import { textParser } from '@/app/components/text-parser';
+
+const studySVGs = [
+  <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10,50 90,50 M50,10 10,50 50,90" strokeWidth="5" strokeLinecap="round" stroke="var(--color-front)" fill="none" />
+  </svg>,
+  <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10,50 90,50 M50,10 90,50 50,90" strokeWidth="5" strokeLinecap="round" stroke="var(--color-front)" fill="none" />
+  </svg>,
+  <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10,10 90,90 M10,90 90,10" strokeWidth="7.5" strokeLinecap="round" stroke="var(--color-back)" fill="none" />
+  </svg>,
+  <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10,50 40,90 90,10" strokeWidth="7.5" strokeLinecap="round" stroke="var(--color-back)" fill="none" />
+  </svg>,
+];
 
 function getVocab() {
   let allVocab: phaseType['items'][number][] = [];
@@ -17,9 +33,17 @@ function getVocab() {
 }
 
 export default function Study() {
-  const [word, setWord] = useState<phaseType['items'][number] | trilingualType>({qu: 'alli', en: 'good', es: 'bueno'});
+  const isFirstRender = useRef(true);
+  // top buttons
+  const [range, setRange] = useState({ start: 0, end: lessons.length-1, });
+  const [showRange, setShowRange] = useState(false);
+  // flash card contents
+  const [word, setWord] = useState({ i: 0, options: getVocab().flat(), });
+  const [prevIndex, setPrevIndex] = useState<number[]>([]);
+  const [nextIndex, setNextIndex] = useState<number[]>([]);
+  //text input
   const [input, setInput] = useState('');
-  const [borderColor, setBorderColor] = useState('border-[var(--color-mid)]');
+  const [correctGuess, setCorrectGuess] = useState<boolean | null>(null);
   const [buttonText, setButtonText] = useState('?');
   const vocab = getVocab();
 
@@ -36,10 +60,43 @@ export default function Study() {
     )
   };
 
+  const updateRange = (e: React.MouseEvent<HTMLLIElement>, key: 'lower' | 'upper') => {
+    const clickedIndex: number = Number(e.currentTarget.textContent)-1;
+    if (key==='lower') {
+      if (clickedIndex >= range.end) { setRange({ start: clickedIndex, end: clickedIndex, }); }
+      else { setRange({ start: clickedIndex, end: range.end, }); }
+    } else if (key==='upper') {
+      setRange({ start: range.start, end: clickedIndex, });
+    } setShowRange(false); setPrevIndex([]); setNextIndex([]);
+  };
+  const toggleShowRange = () => {
+    setShowRange(!showRange);
+  };
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+    const vocabSet = (range.start===range.end ? vocab[range.start] : vocab.slice(range.start,range.end).flat()) as any[];
+    const index = Math.floor(Math.random()*vocabSet.length);
+    setWord({ i: index, options: vocabSet, });
+  }, [range]);
+
+  function oldWord() {
+    if (prevIndex.length>0) {
+      const vocabSet = (range.start===range.end ? vocab[range.start] : vocab.slice(range.start,range.end).flat()) as any[];
+      const index = prevIndex[prevIndex.length-1];
+      setPrevIndex(priorArray => priorArray.slice(0,-1));
+      setNextIndex(priorArray => [...priorArray, word.i]);
+      setWord({ i: index, options: vocabSet, });
+    }
+  }
   function newWord() {
-    const allVocab = vocab.flat();
-    const randomIndex = Math.floor(Math.random() * allVocab.length);
-    setWord(allVocab[randomIndex]);
+    const vocabSet = (range.start===range.end ? vocab[range.start] : vocab.slice(range.start,range.end).flat()) as any[];
+    const index = nextIndex.length>0 ? nextIndex[nextIndex.length-1] : Math.floor(Math.random()*vocabSet.length);
+    setPrevIndex(priorArray => [...priorArray, word.i]);
+    if (nextIndex.length>0) { setNextIndex(priorArray => priorArray.slice(0,-1)); }
+    setWord({ i: index, options: vocabSet, });
   };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -47,30 +104,71 @@ export default function Study() {
   };
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (input==='') { setBorderColor('border-[var(--color-mid)]'); }
-    else if (typeof word === 'object' && word !== null && 'en' in word) {
-      if (word.en.includes(input) || word.es.includes(input)) {
-        setBorderColor('border-[#5d854c]');
-        setButtonText('✓');
-      } else { setBorderColor('border-[#e85538]'); setButtonText('✗'); }
-    } else { setBorderColor('border-[#e85538]'); setButtonText('✗'); }
+    const currentWord = word.options[word.i] as any;
+    if (input==='') { setCorrectGuess(null); }
+    else if (typeof currentWord === 'object' && currentWord !== null && 'en' in currentWord) {
+      if (currentWord.en.includes(input) || currentWord.es.includes(input)) {
+        setCorrectGuess(true);
+        newWord();
+      } else { setCorrectGuess(false); }
+    } else { setCorrectGuess(false); }
     setInput('');
     setTimeout(() => {
-      setBorderColor('border-[var(--color-mid)]');
-      setButtonText('?');
+      setCorrectGuess(null);
     }, 750);
   };
   return (
     <>
       <div className={`${styles.studyWrapper}`}>
         <div className={`${styles.buttonWrap}`}>
-          <button onClick={newWord} className={`${styles.flashButton} mb-4 bg-[var(--color-mid)] rounded-full cursor-pointer`}>⇄</button>
+          <button onClick={oldWord}
+            className={`${styles.flashButton} ${styles.flashBack} ${prevIndex.length===0?'hidden':'block'}`}
+          >{studySVGs[0]}</button>
+          <button className={`${styles.dummy} ${prevIndex.length===0?'block':'hidden'} opacity-0`}></button>
+          <div>
+            <div className={`${montserrat.className} font-extrabold mb-0`}>
+              <Text textObj={{en: `LESSON${range.start<range.end?'S':''}`, es: `LECCIÓN${range.start<range.end?'ES':''}`}} />
+            </div>
+            <div className={`${montserrat.className} font-extrabold text-3xl relative`} onMouseLeave={() => setShowRange(false)}>
+              <button className={styles.rangeSelect} onClick={toggleShowRange}>
+                {range.start<range.end?<><span>{range.start+1}</span>
+                <span>–</span>
+                <span>{range.end+1}</span></>:<><span>{range.start+1}</span></>}
+                <span className={`${styles.dropCaret} ${showRange?styles.upCaret:''}`}>⌄</span>
+              </button>
+              <div className={`${styles.dropdown} ${showRange?'flex':'hidden'}`}>
+                <ul>
+                  {Array.from({ length: lessons.length }, (_, i) => i)
+                    .filter(i => i!==range.start)
+                    .map(i => (
+                      <li key={`lowerBound${i}`} onClick={(e) => updateRange(e,'lower')}>{i + 1}</li>
+                    ))}
+                </ul>
+                {range.start!==9?<ul>
+                  {Array.from({ length: lessons.length }, (_, i) => i)
+                    .filter((i) => i >= range.start && i !== range.end)
+                    .map((i) => (
+                      <li key={`upperBound${i}`} onClick={(e) => updateRange(e, 'upper')}>{i + 1}</li>
+                    ))}
+                </ul>:<></>}
+              </div>
+            </div>
+          </div>
+          <button onClick={newWord} className={`${styles.flashButton} ${styles.flashBack}`}>{studySVGs[1]}</button>
         </div>
-        <FlashCard word={word} />
+        <FlashCard word={word.options[word.i]} />
         <form onSubmit={handleSubmit} className={styles.form}>
           <label>
-            <input type="text" value={input} onChange={handleChange} className={`border-2 border-solid ${borderColor}`} />
-            <button type="submit">{buttonText}</button>
+            <input
+              type="text"
+              value={input}
+              onChange={handleChange}
+              className={`border-2 border-solid ${correctGuess===null?'border-[var(--color-mid)]':correctGuess?'border-[#5d854c]':'border-[#e85538]'}`}
+            />
+            <button
+              type="submit"
+              className={`${correctGuess===null?styles.regButton:correctGuess?styles.yButton:styles.nButton} ${correctGuess===null?'':styles.feedText}`}
+            >{correctGuess===null?'?':correctGuess?studySVGs[3]:studySVGs[2]}</button>
           </label>
         </form>
       </div>
